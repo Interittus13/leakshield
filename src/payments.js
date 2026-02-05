c0e9955ee9be5c21241b91d6a391535d253efc01
arpit.gupta <arpit.gupta@systango.com>
2026-02-05 18:39:13 +0530

    removed secrets

--- src/payments.js
+++ src/payments.js
@@ -1,11 +1,6 @@
 // This file contains JWT + private key block
 // Both should trigger gitleaks
 
-export const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4ifQ.sflkjsdlfkjsdlfkjsdlfkjsdlfkjsdlfk";
+// export const token = "";
 
-export const PRIVATE_KEY = `
------BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAvFakeKeyForTestingOnlyDontUse
------END RSA PRIVATE KEY-----
-`;
+// export const PRIVATE_KEY = ``;
