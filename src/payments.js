b14542568c9b81f1c2149cb0638267e69efffc90
arpit.gupta <arpit.gupta@systango.com>
2026-02-05 17:28:58 +0530

    added secrts

--- src/payments.js
+++ src/payments.js
@@ -5,5 +5,7 @@ export const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4ifQ.sflkjsdlfkjsdlfkjsdlfkjsdlfkjsdlfk";
 
 export const PRIVATE_KEY = `
-
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAvFakeKeyForTestingOnlyDontUse
+-----END RSA PRIVATE KEY-----
 `;
