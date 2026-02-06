57e3c795dc711b0be217f739364606adb1bd8c68
arpit.gupta <arpit.gupta@systango.com>
2026-02-06 11:59:48 +0530

    added stript key

--- src/aws.js
+++ src/aws.js
@@ -4,4 +4,5 @@
 export const AWS = {
   accessKeyId: "AKIAIOSFODNN7EXAMPLE",
   secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  STRIPE_SECRET_KEY:"sk_test_51H8sdfhksdfhsdfhsdfhsdfhsdf"
 };
