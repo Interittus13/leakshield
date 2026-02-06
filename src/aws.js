4c69e8b714147815879370d1447d454ca6760225
arpit.gupta <arpit.gupta@systango.com>
2026-02-06 12:34:40 +0530

    Updated aws.js again x3

--- src/aws.js
+++ src/aws.js
@@ -5,5 +5,10 @@ export const AWS = {
   accessKeyId: "AKIA4F7XXAMPSE12345",
   secretAccessKey: "QwErTyUiOWDsWfGhJkLzXcVbNm1234567890AbCd",
   STRIPE_SECRET_KEY:"sk_test_99H8sdfhksdfhsdfhsdfhsdfhsdf",
-  GITHUB_TOKEN:"ghp_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r"
+  GITHUB_TOKEN:"ghp_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r",
+  PRIVATE_KEY: `
+  -----BEGIN PRIVATE KEY-----
+abc123superlongrandomstringwithentropyvalue987654321
+-----END PRIVATE KEY-----
+`
 };
