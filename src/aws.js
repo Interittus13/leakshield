e0bfd6af1fd14d08b69aab3416611fd643e0b28e
sauravbajpai007 <sauravbajpai06@gmail.com>
2026-02-10 19:30:46 +0530

    Update AWS secret access key and private key

--- src/aws.js
+++ src/aws.js
@@ -1,7 +1,7 @@
 // Fake AWS credentials for testing
 
 const AWS_ACCESS_KEY_ID = "AKIA12355765691ABCD";
-const AWS_SECRET_ACCESS_KEY = "abcd1534dbcd1234dbcd1234abcd4122bccd2134";
+const AWS_SECRET_ACCESS_KEY = "abcd1534dbcd1234dbcd1234abcd4122abcd2134";
 
 // Fake PEM key (for testing)
 const AWS_SESSION_TOKEN = `
@@ -9,4 +9,4 @@ const AWS_SESSION_TOKEN = `
 abc123superlongrandomStringentropysdlue987654321
 -----END PRIVATE KEY-----
 `;
-privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d4299dcc7e225d737f54fb"
+privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d2499dcc7e225d737f54fb"
