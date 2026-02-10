91c2f0dec1ebe1af243507d616a514d380bcb03c
sau <sauravbajpai06@gmail.com>
2026-02-10 18:51:52 +0530

    test gitleaks

--- src/aws.js
+++ src/aws.js
@@ -1,13 +1,12 @@
-// This file simulates leaked AWS credentials
-// Gitleaks default rules always catch these
+// Fake AWS credentials for testing
 
-// # AWS Access Key (FAKE - for testing only)
-AWS_ACCESS_KEY_ID="AKIA9F8D6S5A6Q4s3E2R"
-AWS_SECRET_ACCESS_KEY="QwErTsSi5pAsDfgHLkLzXcVbNm12345opop980AbCd"
-privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d4299dcc7e225d737f54fb"
+const AWS_ACCESS_KEY_ID = "AKIA1234567890ABCD";
+const AWS_SECRET_ACCESS_KEY = "abcd1234abcd1234abcd1234abcd1234abcd1234";
 
-// # AWS Session Token (FAKE - for testing only)
-AWS_SESSION_TOKEN = `
+// Fake PEM key (for testing)
+const AWS_SESSION_TOKEN = `
 -----BEGIN PRIVATE KEY-----
-abc123superlongrandomstrinGsIshenjropyvalue987654321
------END PRIVATE KEY-----`
+abc123superlongrandomstringentropyvalue987654321
+-----END PRIVATE KEY-----
+`;
+privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d4299dcc7e225d737f54fb"
