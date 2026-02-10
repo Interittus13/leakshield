b9d8cf388cef4a6540f136b052059a643b0ea179
sau <sauravbajpai06@gmail.com>
2026-02-10 18:43:28 +0530

    test gitleaks

--- src/aws.js
+++ src/aws.js
@@ -3,7 +3,7 @@
 
 // # AWS Access Key (FAKE - for testing only)
 AWS_ACCESS_KEY_ID="AKIA9F8D6S5A6Q4s3E2R"
-AWS_SECRET_ACCESS_KEY="QwErTsSi5pAsDfgHLkLzXcVbNm12345opop890AbCd"
+AWS_SECRET_ACCESS_KEY="QwErTsSi5pAsDfgHLkLzXcVbNm12345opop980AbCd"
 privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d4299dcc7e225d737f54fb"
 
 // # AWS Session Token (FAKE - for testing only)
