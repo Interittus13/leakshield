bed6b6ecace43e1de672cfc8b3478599ff0699a8
arpit.gupta <arpit.gupta@systango.com>
2026-02-10 14:33:10 +0530

    added smtp

--- src/aws.js
+++ src/aws.js
@@ -5,7 +5,7 @@ export const AWS = {
   accessKeyId: "AKIA4F7XXAMPSE12345",
   secretAccessKey: "QwErTyUiOWDsWfGhJkLzXcVbNm1234567890AbCd",
   STRIPE_SECRET_KEY:"sk_test_99H8sffhksdfhsdfhsdfhsdfhsdf",
-  GITHUB_TOKEN:"ghp_a1b2c3d4e5f6g7h9i9j0k1l2m3n4o5p6q7r",
+  GITHUB_TOKEN:"ghp_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r",
   PRIVATE_KEY: `
   -----BEGIN PRIVATE KEY-----
 abc123superlongrandomstringwithentropyvalue987654321
