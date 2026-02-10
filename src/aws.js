907d4ac3c7b9b531c60bcdb1ae1d1715bde4636a
arpit.gupta <arpit.gupta@systango.com>
2026-02-10 17:49:14 +0530

    updated configs

--- src/aws.js
+++ src/aws.js
@@ -2,11 +2,11 @@
 // Gitleaks default rules always catch these
 
 // # AWS Access Key (FAKE - for testing only)
-AWS_ACCESS_KEY_ID="AKIA9F8D7S5A5Q4s3E2R"
-AWS_SECRET_ACCESS_KEY="QwErTysiOpAsDfgHLkLzXcVbNm1234567890AbCd"
+AWS_ACCESS_KEY_ID="AKIA9F8D6S5A5Q4s3E2R"
+AWS_SECRET_ACCESS_KEY="QwErTssiOpAsDfgHLkLzXcVbNm1234567890AbCd"
 
 // # AWS Session Token (FAKE - for testing only)
 AWS_SESSION_TOKEN = `
 -----BEGIN PRIVATE KEY-----
-abc123superlongrandomstringwishenjropyvalue987654321
+abc123superlongrandomstringwIshenjropyvalue987654321
 -----END PRIVATE KEY-----`
