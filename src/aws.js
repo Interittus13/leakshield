d53e0c8d1270e8af70413154e088721e52934876
sauravbajpai007 <sauravbajpai06@gmail.com>
2026-02-10 19:07:35 +0530

    secrets

--- src/aws.js
+++ src/aws.js
@@ -1,12 +1,12 @@
 // Fake AWS credentials for testing
 
-const AWS_ACCESS_KEY_ID = "AKIA1234567591ABCD";
-const AWS_SECRET_ACCESS_KEY = "abcd1234dbcd1234dbcd1234abcd1234abcd1234";
+const AWS_ACCESS_KEY_ID = "AKIA1235567591ABCD";
+const AWS_SECRET_ACCESS_KEY = "abcd1534dbcd1234dbcd1234abcd1234abcd1234";
 
 // Fake PEM key (for testing)
 const AWS_SESSION_TOKEN = `
 -----BEGIN PRIVATE KEY-----
-abc123superlongrandomStringentropyvdlue987654321
+abc123superlongrandomStringentropysdlue987654321
 -----END PRIVATE KEY-----
 `;
 privateKey:"0x7c9bb647797dd919fe8c2ead70d51483f7b4475c88d4299dcc7e225d737f54fb"
