1216ee8f297cb7645dbb58feea1269196121cf2f
arpit.gupta <arpit.gupta@systango.com>
2026-02-10 16:23:28 +0530

    add leaks

--- src/aws.js
+++ src/aws.js
@@ -2,5 +2,9 @@
 // Gitleaks default rules always catch these
 
 export const AWS = {
-"Hello"
+  a: "AKIA4F7XXAMPSE12345",
+  s: "QwErTyUiOWDsWfGhJkLzXcVbNm1234567890AbCd",
+  STP:"sk_test_99H8sffhksdfhsdfhsdfhsdfhsdf",
+  GTH:"ghp_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r",
+  TEST: "abc123superlongrandomstringwithentropyvalue987654321"
 };
